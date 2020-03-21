f0e8f69c2afeab0e1761a71af5cff031dc535b3e
Hayato Kakiuchi <qweaty.uiop8810@i.softbank.jp>
2020-03-21 17:17:38 +0900

    delete file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+
+let message: string = "helloworld" 
 
 function App() {
   return (
     <div>
-      hello
+      {message}
     </div>
   );
 }
